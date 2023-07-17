c91319a1af36d13990b8f833ff25a1248eaff8a3
karosis88 <kar.petrosyanpy@gmail.com>
2023-07-17 23:29:00 +0000

    Add tests for extract_header_values

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,6 +2,7 @@
 from httpcore import Request
 
 from hishel._utils import generate_key
+from hishel._utils import extract_header_values
 
 
 def test_generate_key():
@@ -40,3 +41,26 @@ def test_generate_key_with_vary():
         "GETURL(scheme=b'https', host=b'example.com', port=None, "
         "target=b'/')Content-Type=application/jsonAccept=application/json"
     )
+
+
+def test_extract_header_values():
+    headers = [
+        (b'Content-Type', b'application/json'),
+        (b'Content-Type', b'application/html')
+    ]
+
+    values = extract_header_values(
+        headers, b'Content-Type'
+    )
+    assert values == [b'application/json', b'application/html']
+
+def test_extract_header_single_value():
+    headers = [
+        (b'Content-Type', b'application/json'),
+        (b'Content-Type', b'application/html')
+    ]
+
+    values = extract_header_values(
+        headers, b'Content-Type', single=True
+    )
+    assert values == [b'application/json']
