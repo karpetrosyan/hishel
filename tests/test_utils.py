bd015bd7e05164be922c032683e30f77c43b58ca
karosis88 <kar.petrosyanpy@gmail.com>
2023-07-18 12:31:43 +0300

    Add tests for parse_date and get_updated_headers

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,8 +8,10 @@ from hishel._utils import (
     extract_header_values,
     extract_header_values_decoded,
     generate_key,
+    get_updated_headers,
     header_presents,
     load_path_map,
+    parse_date
 )
 
 
@@ -113,3 +115,31 @@ def test_load_path_map(use_temp_dir):
     assert len(maps) == 2
     assert maps['first_path'] == Path('/home/test')
     assert maps['second_path'] == Path('test')
+
+
+def test_get_updated_headers():
+
+    old_headers = [
+        (b'Content-Type', b'application/json'),
+        (b'Language', b'en')
+    ]
+
+    new_headers = [
+        (b'Language', b'am'),
+        (b'Content-Length', b"1024"),
+        (b'Authorization', b'secret-key')
+    ]
+
+    update_headers = get_updated_headers(stored_response_headers=old_headers, new_response_headers=new_headers)
+
+    assert len(update_headers) == 3
+    assert extract_header_values(update_headers, b'Language')[0] == b'am'
+    assert extract_header_values(update_headers, b'Content-Type')[0] == b'application/json'
+    assert extract_header_values(update_headers, b'Authorization')[0] == b'secret-key'
+
+
+def test_parse_date():
+
+    date = "Mon, 25 Aug 2015 12:00:00 GMT"
+    timestamp = parse_date(date)
+    assert timestamp == 1440504000
