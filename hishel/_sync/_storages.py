ed3e6735e1ecff9597bf5a2fa9855b3496bf62d8
karosis88 <kar.petrosyanpy@gmail.com>
2023-10-24 17:42:43 +0300

    Add support for SQLite backend

--- hishel/_sync/_storages.py
+++ hishel/_sync/_storages.py
@@ -3,6 +3,7 @@ import time
 import typing as tp
 from pathlib import Path
 
+import sqlite3
 from httpcore import Request, Response
 
 from hishel._serializers import BaseSerializer
@@ -13,7 +14,7 @@ from .._synchronization import Lock
 
 logger = logging.getLogger("hishel.storages")
 
-__all__ = ("FileStorage", "RedisStorage")
+__all__ = ("FileStorage", "RedisStorage", "SQLiteStorage")
 
 try:
     import redis
@@ -134,6 +135,126 @@ class FileStorage(BaseStorage):
                     age = time.time() - file.stat().st_mtime
                     if age > self._ttl:
                         file.unlink()
+
+
+class SQLiteStorage(BaseStorage):
+    """
+    A simple sqlite3 storage.
+
+    :param serializer: Serializer capable of serializing and de-serializing http responses, defaults to None
+    :type serializer: tp.Optional[BaseSerializer], optional
+    :param connection: A connection for sqlite, defaults to None
+    :type connection: tp.Optional[sqlite3.Connection], optional
+    :param ttl: Specifies the maximum number of seconds that the response can be cached, defaults to None
+    :type ttl: tp.Optional[int], optional
+    """
+
+    def __init__(
+        self,
+        serializer: tp.Optional[BaseSerializer] = None,
+        connection: tp.Optional[sqlite3.Connection] = None,  # type: ignore
+        ttl: tp.Optional[int] = None,
+    ) -> None:
+        if sqlite3 is None:  # pragma: no cover
+            raise RuntimeError(
+                (
+                    f"The `{type(self).__name__}` was used, but the required packages were not found. "
+                    "Check that you have `Hishel` installed with the `sqlite` extension as shown.\n"
+                    "```pip install hishel[sqlite]```"
+                )
+            )
+        super().__init__(serializer)
+
+        self._connection: tp.Optional[sqlite3.Connection] = connection or None
+        self._setup_lock = Lock()
+        self._setup_completed: bool = False
+        self._lock = Lock()
+        self._ttl = ttl
+
+    def _setup(self) -> None:
+        with self._setup_lock:
+            if not self._setup_completed:
+                self._connection = sqlite3.connect(".hishel.sqlite")
+                self._connection.execute(
+                    (
+                        "CREATE TABLE IF NOT EXISTS cache(key TEXT, data BLOB, "
+                        "date_created datetime DEFAULT CURRENT_TIMESTAMP)"
+                    )
+                )
+                self._connection.commit()
+                self._setup_completed = True
+
+    def store(
+        self, key: str, response: Response, request: Request, metadata: Metadata
+    ) -> None:
+        """
+        Stores the response in the cache.
+
+        :param key: Hashed value of concatenated HTTP method and URI
+        :type key: str
+        :param response: An HTTP response
+        :type response: httpcore.Response
+        :param request: An HTTP request
+        :type request: httpcore.Request
+        :param metadata: Additioal information about the stored response
+        :type metadata: Metadata
+        """
+
+        self._setup()
+        assert self._connection
+
+        with self._lock:
+            self._connection.execute("DELETE FROM cache WHERE key = ?", [key])
+            serialized_response = self._serializer.dumps(
+                response=response, request=request, metadata=metadata
+            )
+            self._connection.execute(
+                "INSERT INTO cache(key, data) VALUES(?, ?)", [key, serialized_response]
+            )
+            self._connection.commit()
+        self._remove_expired_caches()
+
+    def retreive(
+        self, key: str
+    ) -> tp.Optional[tp.Tuple[Response, Request, Metadata]]:
+        """
+        Retreives the response from the cache using his key.
+
+        :param key: Hashed value of concatenated HTTP method and URI
+        :type key: str
+        :return: An HTTP response and its HTTP request.
+        :rtype: tp.Optional[tp.Tuple[Response, Request, Metadata]]
+        """
+
+        self._setup()
+        assert self._connection
+
+        with self._lock:
+            cursor = self._connection.execute(
+                "SELECT data FROM cache WHERE key = ?", [key]
+            )
+            row = cursor.fetchone()
+            if row is None:
+                return None
+
+            cached_response = row[0]
+            return self._serializer.loads(cached_response)
+        self._remove_expired_caches()
+
+    def close(self) -> None:  # pragma: no cover
+        assert self._connection
+        self._connection.close()
+
+    def _remove_expired_caches(self) -> None:
+        assert self._connection
+        if self._ttl is None:
+            return
+
+        with self._lock:
+            self._connection.execute(
+                f"DELETE FROM cache WHERE datetime(date_created, '+{self._ttl} seconds') > datetime()"
+            )
+            self._connection.commit()
 
 
 class RedisStorage(BaseStorage):
