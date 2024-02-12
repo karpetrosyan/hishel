9d8db47cd23f2572ddc0aea12593381da1e64cce
Ilia <30232564+umnovI@users.noreply.github.com>
2024-02-12 19:29:18 +0300

    Make `S3Storage` to check staleness of all cache files with set interval. (#182)
    * Make `S3Storage` to check staleness of all cache files with set interval.
    
    * Change description for `test_s3storage_key_error`
    
    * Run checks.
    
    * Remove unnecessary try/except
    
    * Update CHANGELOG.md
    
    * Increase `ttl` for `test_s3storage_expired`
    
    * Update storages.md
    
    Add `check_ttl_every` example for `S3Storage`
    
    ---------
    
    Co-authored-by: Kar Petrosyan <92274156+karpetrosyan@users.noreply.github.com>

--- hishel/_s3.py
+++ hishel/_s3.py
@@ -1,14 +1,20 @@
+import time
 import typing as tp
 from datetime import datetime, timedelta, timezone
 
 from anyio import to_thread
+from botocore.exceptions import ClientError
 
 
 class S3Manager:
-    def __init__(self, client: tp.Any, bucket_name: str, is_binary: bool = False):
+    def __init__(
+        self, client: tp.Any, bucket_name: str, check_ttl_every: tp.Union[int, float], is_binary: bool = False
+    ):
         self._client = client
         self._bucket_name = bucket_name
         self._is_binary = is_binary
+        self._last_cleaned = time.monotonic()
+        self._check_ttl_every = check_ttl_every
 
     def write_to(self, path: str, data: tp.Union[bytes, str]) -> None:
         path = "hishel-" + path
@@ -31,7 +37,21 @@ class S3Manager:
 
         return tp.cast(str, content.decode("utf-8"))
 
-    def remove_expired(self, ttl: int) -> None:
+    def remove_expired(self, ttl: int, key: str) -> None:
+        path = "hishel-" + key
+
+        if time.monotonic() - self._last_cleaned < self._check_ttl_every:
+            try:
+                response = self._client.get_object(Bucket=self._bucket_name, Key=path)
+                if datetime.now(timezone.utc) - response["LastModified"] > timedelta(milliseconds=ttl):
+                    self._client.delete_object(Bucket=self._bucket_name, Key=path)
+                return
+            except ClientError as e:
+                if e.response["Error"]["Code"] == "NoSuchKey":
+                    return
+                raise e
+
+        self._last_cleaned = time.monotonic()
         for obj in self._client.list_objects(Bucket=self._bucket_name).get("Contents", []):
             if not obj["Key"].startswith("hishel-"):  # pragma: no cover
                 continue
@@ -41,8 +61,10 @@ class S3Manager:
 
 
 class AsyncS3Manager:
-    def __init__(self, client: tp.Any, bucket_name: str, is_binary: bool = False):
-        self._sync_manager = S3Manager(client, bucket_name, is_binary)
+    def __init__(
+        self, client: tp.Any, bucket_name: str, check_ttl_every: tp.Union[int, float], is_binary: bool = False
+    ):
+        self._sync_manager = S3Manager(client, bucket_name, check_ttl_every, is_binary)
 
     async def write_to(self, path: str, data: tp.Union[bytes, str]) -> None:
         return await to_thread.run_sync(self._sync_manager.write_to, path, data)
@@ -50,5 +72,5 @@ class AsyncS3Manager:
     async def read_from(self, path: str) -> tp.Union[bytes, str]:
         return await to_thread.run_sync(self._sync_manager.read_from, path)
 
-    async def remove_expired(self, ttl: int) -> None:
-        return await to_thread.run_sync(self._sync_manager.remove_expired, ttl)
+    async def remove_expired(self, ttl: int, key: str) -> None:
+        return await to_thread.run_sync(self._sync_manager.remove_expired, ttl, key)
