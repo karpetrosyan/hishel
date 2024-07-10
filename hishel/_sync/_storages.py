754cf9dabdb0fa8e7109e9d0953133f4650318f6
Simon Will <gorgor@gorgor.de>
2024-07-10 23:56:57 +0200

    Make file cache scanning more efficient (#253)
    By using `os.scandir`, we can save the syscall for `is_file`.

--- hishel/_sync/_storages.py
+++ hishel/_sync/_storages.py
@@ -203,11 +203,12 @@ class FileStorage(BaseStorage):
 
         self._last_cleaned = time.monotonic()
         with self._lock:
-            for file in self._base_path.iterdir():
-                if file.is_file():
-                    age = time.time() - file.stat().st_mtime
-                    if age > self._ttl:
-                        file.unlink()
+            with os.scandir(self._base_path) as entries:
+                for entry in entries:
+                    if entry.is_file():
+                        age = time.time() - entry.stat().st_mtime
+                        if age > self._ttl:
+                            os.unlink(entry.path)
 
 
 class SQLiteStorage(BaseStorage):
