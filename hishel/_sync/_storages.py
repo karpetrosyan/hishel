f79f485f9723b23375b28f0ca2d7319ff3894f27
Florian Daude <floriandaude@hotmail.fr>
2024-08-28 12:56:47 +0200

    Ignoring error when removing expired file storage cache to not fail on race conditions

--- hishel/_sync/_storages.py
+++ hishel/_sync/_storages.py
@@ -227,10 +227,13 @@ class FileStorage(BaseStorage):
         with self._lock:
             with os.scandir(self._base_path) as entries:
                 for entry in entries:
-                    if entry.is_file():
-                        age = time.time() - entry.stat().st_mtime
-                        if age > self._ttl:
-                            os.unlink(entry.path)
+                    try:
+                        if entry.is_file():
+                            age = time.time() - entry.stat().st_mtime
+                            if age > self._ttl:
+                                os.unlink(entry.path)
+                    except FileNotFoundError:  # pragma: no cover
+                        pass
 
 
 class SQLiteStorage(BaseStorage):
