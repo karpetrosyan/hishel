4986179b7c41385da0ad79910353c87afa485a53
Kar Petrosyan <92274156+karpetrosyan@users.noreply.github.com>
2024-11-02 13:03:17 +0400

    FIx typig extensions nor found (#290)
    * FIx typig extensions nor found
    
    * Changelog
    
    * Fix coverage

--- hishel/_async/_storages.py
+++ hishel/_async/_storages.py
@@ -23,7 +23,9 @@ except ImportError:  # pragma: no cover
     anysqlite = None  # type: ignore
 
 from httpcore import Request, Response
-from typing_extensions import TypeAlias
+
+if t.TYPE_CHECKING:  # pragma: no cover
+    from typing_extensions import TypeAlias
 
 from hishel._serializers import BaseSerializer, clone_model
 
